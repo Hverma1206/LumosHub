c38b73bb8def4d9f17e9015701b50b66e9a99073
hverma1206 <hv308489@example.com>
2025-11-01 17:16:58 +0530

    vercel insights

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="app-container">
       <Outlet />
+      <Analytics />
     </div>
   )
 }
